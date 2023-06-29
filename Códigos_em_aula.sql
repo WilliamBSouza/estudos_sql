ab25e14c61b2928e4bae337e7f9f530bd376b94b
William Souza <williambarrosdesouza34@gmail.com>
2023-06-29 20:21:58 -0300

    aula 3

--- Códigos_em_aula.sql
+++ Códigos_em_aula.sql
@@ -31,6 +31,8 @@ FROM clientes;
 
 SELECT * FROM produtos LIMIT 5;
 
+
+
 -- SELECIONAR TODAS AS LINHAS DA TABELA PRODUTOS MAS... 
 
 -- ORDENANDO PELA COLUNA PREÇO_UNIT 
@@ -38,4 +40,31 @@ SELECT * FROM produtos LIMIT 5;
 SELECT * FROM produtos ORDER BY Preco_Unit ; -- ORDENA DO MENOR PARA O MAIOR 
 
 SELECT * FROM produtos ORDER BY Preco_Unit DESC ; -- ORDENA DO MAIOR PARA O MENOR 
+
+
+
+-- Aula 3 WHERE 
+-- pesquisa os produtos com preço igual ou maior que 1800 reais		
+SELECT* FROM produtos WHERE Preco_Unit >= 1800;
+
+-- pesquisa os produtos com o valor de 3000 reais 
+SELECT* FROM produtos where Preco_Unit = 3100;
+
+-- pesquisa apenas os produtos da marca dell
+SELECT* FROM produtos WHERE Marca_Produto ='DELL';
+
+-- PESQUISA OS PEDIDOS VENDIDOS NO DIA 03/01/2019
+
+SELECT * FROM pedidos WHERE Data_venda ='2019-01-03';
+
+-- MOSTRA APENAS SOLTEIROS E SEXO MASCULINO 
+SELECT* FROM clientes WHERE Estado_Civil = 's' AND Sexo = 'M';
+
+-- MOSTRA APENAS OS PRODUTOS DA DELL OU SAMSUNG
+SELECT * FROM produtos WHERE Marca_Produto ='DELL' OR Marca_Produto ='SAMSUNG';
+
+ 
+ 
+
+
 
